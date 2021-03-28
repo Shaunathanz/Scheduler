20f4258f1067ed512635027852b0aec65cd9cf95
Shaun Graham <shaunkiliangraham@gmail.com>
2021-03-28 17:52:11 -0500

    Partial Merge

--- public_html/shared/tutor-nav.php
+++ public_html/shared/tutor-nav.php
@@ -4,5 +4,6 @@
         <li onclick="goToPage('tutor-appointments');">Appointments</li>
         <li onclick="goToPage('tutor-sections');">Sections</li>
         <li onclick="goToPage('tutor-profile');">Profile</li>
+        <li onclick="goToPage('/');">Logout</li>
     </ul>
 </div>
