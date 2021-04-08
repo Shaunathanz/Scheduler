0059f3d054a3565edfbb91ff300106035ae3735f
Shaun Graham <shaunkiliangraham@gmail.com>
2021-04-07 21:10:41 -0500

    Small Updates

--- public_html/shared/tutor-nav.php
+++ public_html/shared/tutor-nav.php
@@ -4,6 +4,7 @@
     <ul id="tutor-nav">
         <li onclick="showPopup('calendar');">[TEMP] Calendar</li> <!-- -->
         <li onclick="showPopup('appointment');">[TEMP] Appointments</li> <!--These will be useless soon -->
+        <li onclick="showPopup('availability');">[To Do] Availability</li>        
         <li onclick="showPopup('profile');">Profile</li>
         <li onclick="goToPage('splash');">Logout</li>
     </ul>
