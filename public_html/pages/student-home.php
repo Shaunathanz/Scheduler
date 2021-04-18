f814d2afdbb2a2a559560564fc8bde54b8f38efa
Shaun Graham <shaunkiliangraham@gmail.com>
2021-04-18 18:28:17 -0500

    Update student-home.php

--- public_html/pages/student-home.php
+++ public_html/pages/student-home.php
@@ -7,7 +7,7 @@
     <title id="tab_name">Student</title>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
+  <!--<link rel="stylesheet" href="/resources/demos/style.css">-->
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
