60a991105542143abb5f6b5b5429af0c565e8054
Shaun Graham <shaunkiliangraham@gmail.com>
2021-04-18 16:03:28 -0500

    These credentials work regardless of whose server the site is on
    You'll have to comment my credentials out and use your own with the same format

--- public_html/scripts/database.php
+++ public_html/scripts/database.php
@@ -4,10 +4,7 @@
     require_once('db_credentials.php');
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME); //XAMPP DEFAULT
     if(!confirm_db_connect()) {
-      $connection = mysqli_connect(DB_SERVER2, DB_USER2, DB_PASS2, DB_NAME2); //JASON
-      if(!confirm_db_connect()) {
-        $connection = mysqli_connect(DB_SERVER3, DB_USER3, DB_PASS3, DB_NAME3); //SHAUN
-      }
+      $connection = mysqli_connect(DB_SERVER2, DB_USER2, DB_PASS2, DB_NAME2); //Shaun <-> Jason
     }
     confirm_db_connect();
     return $connection;
--- public_html/scripts/db_credentials.php
+++ public_html/scripts/db_credentials.php
@@ -5,13 +5,13 @@ DEFINE('DB_NAME', 'scheduler');
 DEFINE('DB_USER', 'root');
 DEFINE('DB_PASS', '');
 //JASON
-DEFINE('DB_SERVER2', 'localhost');
-DEFINE('DB_NAME2', 'ics325sp2115');
-DEFINE('DB_USER2', 'ics325sp2115');
-DEFINE('DB_PASS2', '7544');
+// DEFINE('DB_SERVER2', 'localhost');
+// DEFINE('DB_NAME2', 'ics325sp2115');
+// DEFINE('DB_USER2', 'ics325sp2115');
+// DEFINE('DB_PASS2', '7544');
 //SHAUN
-DEFINE('DB_SERVER3', 'localhost');
-DEFINE('DB_NAME3', 'ics325sp2108');
-DEFINE('DB_USER3', 'ics325sp2108');
-DEFINE('DB_PASS3', '3428');
+DEFINE('DB_SERVER2', 'localhost');
+DEFINE('DB_NAME2', 'ics325sp2108');
+DEFINE('DB_USER2', 'ics325sp2108');
+DEFINE('DB_PASS2', '3428');
 ?>
