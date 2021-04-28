c028fd92f94f885c8e44f604250c5cdc32c56cd8
Shaun Graham <shaunkiliangraham@gmail.com>
2021-04-27 19:17:10 -0500

    WIP commit
    Subjects options populate properly

--- public_html/pages/student-home.php
+++ public_html/pages/student-home.php
@@ -46,29 +46,39 @@ $firstname = $lastname = $email = $gender = $comment = $website = $major =  "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	insert_appointment($_POST);
 }
-$sql = "SELECT * FROM Subject";
+$sql = "SELECT DISTINCT subject FROM Subject";
 $result = $db->query($sql);
 
 ?>
 
-<h2>Student Portal</h2> <!-- TODO: Make these fields make sense for the context of the page -->
+<h2>Student Portal</h2>
 <p>Welcome to the student page. We are here to help. Please make your selections below. <br>You will receive a confirmation email once your session has been confirmed or cancelled </p>
 <p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="student-appt-form">  
   Select subjects 
-  <select name="subject" required class="form-select form-control">
+  <select name="subject" required class="form-select form-control" id="subject"?>">
 		<?php
 		while($row = $result->fetch_assoc()) {
 			//print_r($row);
-			$_id = $row['tutor_id'];
 			$subject = $row['subject'];
 			echo "<option value='".$subject."'>".$subject."</option>";
-		}
+    }
 		?>
   </select>
   <span class="error">* <?php echo $firstnameErr;?></span>
   <br><br>
-  Select day: <input type="text" id="datepicker" name="day" value="">
+  Select day: <input type="date" name="day" min="<?php echo date('Y-m-d');?>" value="" id="dateSelector">
+  <script> //Credit to https://stackoverflow.com/questions/49863189/disable-weekends-on-html-5-input-type-date
+    const picker = document.getElementById('dateSelector');
+    picker.addEventListener('input', function(e){
+      var day = new Date(this.value).getUTCDay();
+      if([6,0].includes(day)){
+        e.preventDefault();
+        this.value = '';
+        alert('Weekends not allowed');
+      }
+    });
+  </script>
   <span class="error">* <?php echo $lastnameErr;?></span>
   <br><br>
   Select time in: <input type="text" class="timepicker" name="timein" value="">
@@ -78,16 +88,20 @@ $result = $db->query($sql);
   <span class="error">* <?php echo $emailErr;?></span>
   <br><br>
   
-  Select teacher 
+  Select tutor 
   <select name="teacher" required class="form-select form-control">
 		<?php
-      $sql = "SELECT * FROM Tutor";
+    if(isset($_POST['subject'])){
+      $sql = "SELECT * FROM Tutor INNER JOIN Subject ON Tutor.id = Subject.tutor_id AND Subject.subject = ".$_POST['subject'].";";
+      //echo $sql;
       $result = $db->query($sql);
       while($row = $result->fetch_assoc()) {
         //print_r($row);
-        $_id = $row['id'];
+        $id = $row['id'];
         $name = $row['name'];
+        echo "<option value='".$id."'>".$name."</option>";
       }
+    }
 		?>
   </select>
   <br><br>
