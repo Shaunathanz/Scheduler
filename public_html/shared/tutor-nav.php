7a88062b58abf429a7adfdd3a45f6cb2174111d6
Shaun Graham <shaunkiliangraham@gmail.com>
2021-04-27 20:12:36 -0500

    Last Working Ver

--- public_html/shared/tutor-nav.php
+++ public_html/shared/tutor-nav.php
@@ -1,10 +1,9 @@
 <?php require_once '../scripts/initialize.php'; ?>
-<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="../assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <div id="nav">
     <ul id="tutor-nav">
+        <li onclick="showPopup('calendar');">[TEMP] Calendar</li> <!-- -->
+        <li onclick="showPopup('appointment');">[TEMP] Appointments</li> <!--These will be useless soon -->
         <li onclick="showPopup('availability');">[To Do] Availability</li>        
         <li onclick="showPopup('profile');">Profile</li>
         <li onclick="goToPage('logout');">Logout</li>
